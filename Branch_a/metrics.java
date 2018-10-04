845c5c8cde98498dc5e45b3caf5b0c58f3a5787c
Benny Cai <benny031495@gmail.com>
2018-10-04 07:28:16 -0700

    everything seems to work except for exception handling...

--- Branch_a/metrics.java
+++ Branch_a/metrics.java
@@ -3,8 +3,23 @@ import java.util.Scanner;
 
 public class metrics {
 
-  public static void main(String[] args) throws IOException {
-    File folder = new File( args[1] );
+  public static void main(String[] args) {
+
+    if (args.length == 0) {
+      howToUse();
+      System.exit(0);
+    }
+
+    String arguements = "";
+    if (args[0].contains("-")) {
+      if (args[0].contains("l")) arguements += "l";
+      if (args[0].contains("c")) arguements += "c";
+      if (args[0].contains("w")) arguements += "w";
+    } else {
+      arguements = "lcw";
+    }
+
+    File folder = new File( args[args.length-1] );
     File[] listOfFiles = folder.listFiles();
     for (int i = 0; i < listOfFiles.length; i++) {
       if (listOfFiles[i].isFile()) {
@@ -12,10 +27,10 @@ public class metrics {
         System.out.println("Directory " + listOfFiles[i].getName());
       }
     }
-    fileRead(listOfFiles);
+    fileRead(listOfFiles, arguements);
   }
 
-  public static void fileRead (File listOfFiles[]) throws IOException{
+  public static void fileRead (File listOfFiles[], String arguements) {
     int fileCount = 0;
     while ( fileCount < ((listOfFiles.length))) {
       try (BufferedReader file = new BufferedReader ( new FileReader(listOfFiles[fileCount]))){
@@ -27,13 +42,32 @@ public class metrics {
           line = file.readLine();
           numOfLines++;
         }
+        System.out.println(listOfFiles[fileCount]);
         fileCount++;
         fileAnaylsis readingStuff = new fileAnaylsis(fileObject, numOfLines);
-        System.out.println(readingStuff.toString());
-        System.out.println(readingStuff.getLine() + " lines");
-        System.out.println(readingStuff.getWordCount(fileObject) + " words");
-        System.out.println(readingStuff.splitChar(fileObject) + " chars");
+
+        if (arguements.equals("l")) {
+          System.out.println(readingStuff.getLine() + " lines");
+        } else if (arguements.equals("w")){
+          System.out.println(readingStuff.getWordCount(fileObject) + " words");
+        } else if (arguements.equals("c")){
+          System.out.println(readingStuff.splitChar(fileObject) + " chars");
+        } else {
+          System.out.println(readingStuff.toString());
+        }
+      }
+      catch(Exception e){
+                System.out.println("Please enter a valid directory or file");   //tells user to enter correct directory or file
       }
     }
   }
+
+  public static void howToUse(){
+    System.out.println("-l follow by <filename> will print the line count of a file");
+    System.out.println("-c follow by <filename> will print the character count of a file");
+    System.out.println("-w follow by <filename> will print the word count of a file");
+    System.out.println("<filename> will print all of the above");
+    System.out.println("Please rerun program with the following format");
+  }
+
 }
