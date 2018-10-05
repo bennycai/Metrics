1e32cf57b9eee8b6a304797a9dc04e352759a0ad
Benny Cai <benny031495@gmail.com>
2018-10-04 19:27:26 -0700

    added comments

--- Branch_a/metrics.java
+++ Branch_a/metrics.java
@@ -11,6 +11,8 @@ public class metrics {
     }
 
     String arguements = "";
+    /* supposed to used picocli here but I couldnt figure it out on how to use
+    so i used these types of code instead */
     if (args[0].contains("-")) {
       if (args[0].contains("l")) arguements += "l";
       if (args[0].contains("c")) arguements += "c";
@@ -29,18 +31,26 @@ public class metrics {
         System.out.println("Directory " + listOfFiles[i].getName());
       }
     }
-    File[] codingLanguages = new File[listOfFiles.length];
+    /* This set of code is to get files from certain directories and store them
+    into a listOfFiles array so we can use it later */
 
-    System.out.println(codingLanguages[1]);
+    File[] codingLanguages = new File[listOfFiles.length];
+    /* created an new array to store all the files with programming languages */
+
     languageSort(listOfFiles, codingLanguages);
     if (arguements.contains("l") || arguements.contains("c") || arguements.contains("w")) {
       fileRead(listOfFiles, arguements);
     } else if (arguements.contains("s") || arguements.contains("m")){
       programFileRead(codingLanguages, arguements);
     }
-
+    /* if arguemnt is l c or w then it goes to fileRead method, while if user wants
+    lines of code or comments, it get sent to programFileRead method */
   }
   public static void programFileRead (File codingLanguages[], String arguements){
+    /* wanted this program to only read programming language files, so it uses the
+    codingLanguages array where all the languages are stored. Wanted this to print
+    the number of LOC and the number of comments, but it keeps returning an error... */
+
     int fileCount = 0;
     int i =0;
     while (codingLanguages[i] != null){
@@ -48,7 +58,6 @@ public class metrics {
     }
     System.out.println(i);
     while (fileCount <= (i-1)){
-      System.out.println("inloop?");
       try (BufferedReader files = new BufferedReader (new FileReader(codingLanguages[fileCount]))){
         int numOfComments = 0;
         int numLOC = 0;
@@ -58,25 +67,36 @@ public class metrics {
         while (line !=null ){
           line = files.readLine();
           numOfLines++;
-          if (line.contains("//")){
+          if (line.contains("/")){
             numOfComments ++;
           } else numLOC ++;
         }
+        /*records the number of comments if the line string contains / and
+        if it doesnt not contain it, it will be counted towards a LOC */
+
         System.out.println(codingLanguages[fileCount]);
         if (arguements.equals("s")) {
           System.out.println(numLOC);
         } else if (arguements.equals("m")){
           System.out.println(numOfComments);
         }
+
+        /* Prints the file name and depending on arguments, it will print the
+        LOC or the number of comments. */
+
         fileCount ++;
       }
       catch(Exception e){
               System.out.println("Please enter a valid directory or file");
+              howToUse();
               System.exit(0);
       }
     }
   }
   public static void fileRead (File listOfFiles[], String arguements) {
+    /*methods is basically the first Sprint. It will deal with all the word count,
+    character count and line count.*/
+
     int fileCount = 0;
     while ( fileCount < ((listOfFiles.length))) {
       try (BufferedReader file = new BufferedReader ( new FileReader(listOfFiles[fileCount]))){
@@ -101,23 +121,34 @@ public class metrics {
         } else {
           System.out.println(readingStuff.toString());
         }
+
+        /* I actually turned each file into a String and then send it over to
+        another class to count the number of characters and words. The number of
+        lines is counted in this method. */
       }
       catch(Exception e){
                 System.out.println("Please enter a valid directory or file");
+                howToUse();
                 System.exit(0);
       }
     }
   }
 
   public static void howToUse(){
+    /*tells the user how to use the program in command prompt. */
     System.out.println("-l follow by <filename> will print the line count of a file");
     System.out.println("-c follow by <filename> will print the character count of a file");
     System.out.println("-w follow by <filename> will print the word count of a file");
+    System.out.println("-s follow by <filename> will print the LOC count of a file");
+    System.out.println("-m follow by <filename> will print the comment line count of a file");
     System.out.println("<filename> will print all of the above");
     System.out.println("Please rerun program with the following format");
   }
 
   public static void languageSort (File listOfFiles[], File codingLanguages[]){
+    /* sorts the files that end with java, c, h, cpp, and hpp into codingLanguage
+    array from the listOfFile array so we can see which files are in coding
+    languages formats */
     int numOfProgramingFiles = 0;
     for (int i = 0; i < listOfFiles.length; i++){
       String temp = listOfFiles[i].getName();
